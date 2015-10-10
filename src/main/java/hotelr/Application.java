cf198419988f1c112e139cd5e003e15b0c2e10e2
joaocampinhos <joao.campinhos@gmail.com>
2015-10-10 17:50:58 +0100

    add base project (lab2)

---
+++ src/main/java/hotelr/Application.java
@@ -0,0 +1,47 @@
+package hotelr;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+	
+	/**
+	 * The main() method uses Spring Boot’s SpringApplication.run() method to launch an application.
+	 * The run() method returns an ApplicationContext where all the beans that were created 
+	 * either by your app or automatically added thanks to Spring Boot are.
+	 * @param args
+	 */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Autowired
+    HotelRepository hotels;
+    
+    @Override
+    public void run(String... strings) {
+    	    
+    	log.info("Setting up seed data");
+    	
+    	hotels.deleteAll();
+    	Hotel myHotels[] = {new Hotel(0,"Marriot"), 
+    						new Hotel(1,"Intercontinental"), 
+    						new Hotel(2,"Trip"), 
+    						new Hotel(3,"Holiday Inn"), 
+    						new Hotel(4,"Tulip"), 
+    						new Hotel(5,"Hostel da Costa")};
+    	
+    	for(Hotel hotel : myHotels) hotels.save(hotel);
+    }
+
+}
+
+
---
+++ src/main/java/hotelr/HotelRepository.java
@@ -0,0 +1,13 @@
+package hotelr;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.*;
+
+
+public interface HotelRepository extends CrudRepository<Hotel, Long> {
+	
+	Hotel findByName(String name);
+}
+

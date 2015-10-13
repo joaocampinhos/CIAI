ff312fac9c57d1f3e12de4158e599f0c8e50cf27
pefido <pedrofilipeduraes@hotmail.com>
2015-10-13 16:14:57 +0100

    add price to room

--- src/main/java/hotelr/model/Room.java
+++ src/main/java/hotelr/model/Room.java
@@ -18,8 +18,9 @@ public class Room {
   private long id;
   private Hotel hotel;
   private RoomType type;
+  private int price;
   private int number;
-  
+
   public Room() {}
 
   public Room(long id, Hotel hotel, RoomType type, int number) {
@@ -28,46 +29,55 @@ public class Room {
     this.type = type;
     this.number = number;
   }
-  
+
   @Id
   @Column(name="ROOM_ID")
   public long getId() {
     return id;
   }
-  
+
   public void setId(long id) {
     this.id = id;
   }
-  
+
   @ManyToOne()
   @JoinColumn(name="HOTEL_ID")
   public Hotel getHotel() {
     return hotel;
   }
-  
+
   public void setHotel(Hotel hotel) {
     this.hotel = hotel;
   }
-  
+
   @ManyToOne
   @JoinColumn(name="ROOM_TYPE_ID")
   public RoomType getType() {
     return type;
   }
-  
+
   public void setType(RoomType type) {
     this.type = type;
   }
-  
+
+  @Column(name="ROOM_PRICE")
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
   @Column(name="ROOM_NUMBER")
   public int getNumber() {
     return number;
   }
-  
+
   public void setNumber(int number) {
     this.number = number;
   }
-  
+
   public String toString() {
     return "Id: " + id + "\nHotel: " + hotel.getName() + "\nType: " + type.getName() + "\nNumber: " + getNumber();
   }

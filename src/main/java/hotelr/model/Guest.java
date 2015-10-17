001d1ffb6dfb95e0948c036456d186e07d71f89c
Gonçalo Cabrita <_@gmcabrita.com>
2015-10-17 14:01:27 +0100

    Implement view bookings.
    Start development on the guest dashboard.

--- src/main/java/hotelr/model/Guest.java
+++ src/main/java/hotelr/model/Guest.java
@@ -1,17 +1,31 @@
 package hotelr.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.OneToMany;
+import javax.persistence.FetchType;
 
 @Entity
 @Table(name="GUEST_TABLE")
 public class Guest extends User {
 
+  @OneToMany(mappedBy="guest", targetEntity=Booking.class, fetch=FetchType.EAGER)
+  public List<Booking> bookings;
+
   public Guest() {
     super();
+    this.bookings = new ArrayList<Booking>();
   }
 
   public Guest(long id, String name, String email, String password) {
     super(id, name, email, password);
+    this.bookings = new ArrayList<Booking>();
+  }
+
+  public List<Booking> getBookings() {
+    return this.bookings;
   }
 }

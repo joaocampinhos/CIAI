1c3986fb73fd6c8fc11ab2b942123351b92264c3
Gonçalo Cabrita <_@gmcabrita.com>
2015-10-18 13:40:10 +0100

    Prepare cascades for Manager deletion.

--- src/main/java/hotelr/model/Manager.java
+++ src/main/java/hotelr/model/Manager.java
@@ -1,17 +1,33 @@
 package hotelr.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.OneToMany;
+import javax.persistence.FetchType;
+import javax.persistence.CascadeType;
 
 @Entity
 @Table(name="MANAGER_TABLE")
 public class Manager extends User {
 
+  @OneToMany(mappedBy="manager", cascade=CascadeType.ALL, targetEntity=Hotel.class, fetch=FetchType.LAZY)
+  public List<Hotel> hotels;
+
+  @OneToMany(mappedBy="manager", cascade=CascadeType.ALL, targetEntity=Reply.class, fetch=FetchType.LAZY)
+  public List<Reply> replies;
+
   public Manager() {
     super();
+    this.hotels = new ArrayList<Hotel>();
+    this.replies = new ArrayList<Reply>();
   }
 
   public Manager(long id, String name, String email, String password) {
     super(id, name, email, password);
+    this.hotels = new ArrayList<Hotel>();
+    this.replies = new ArrayList<Reply>();
   }
 }

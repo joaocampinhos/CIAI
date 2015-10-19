4ec1edbe9b5b95ba14dea4fb9b9936d6ae38ce1e
joaocampinhos <joao.campinhos@gmail.com>
2015-10-19 21:57:36 +0100

    new booking com bugs

--- src/main/java/hotelr/web/ManagerDashboardController.java
+++ src/main/java/hotelr/web/ManagerDashboardController.java
@@ -31,6 +31,9 @@ public class ManagerDashboardController {
   RoomRepository rooms;
 
   @Autowired
+  GuestRepository guests;
+
+  @Autowired
   BookingRepository bookings;
 
   @Autowired
@@ -191,6 +194,60 @@ public class ManagerDashboardController {
     } else {
       redirectAttrs.addFlashAttribute("error", "Booking doesn't exist!");
     }
+    return "redirect:/dashboards/manager";
+  }
+
+  @RequestMapping(value="bookings/new",method=RequestMethod.GET)
+  public String newBooking(Model model, RedirectAttributes redirectAttrs) {
+    Manager manager = managers.findByName("O Chefe");
+    model.addAttribute("manager", manager);
+    model.addAttribute("guests", guests.findAll());
+    model.addAttribute("roomtype", roomTypes.findAll());
+    return "dashboards/manager/bookingnew";
+  }
+
+  @RequestMapping(value="bookings",method=RequestMethod.POST)
+  public String update(@RequestParam("hotelid") long hotelid, @RequestParam("guestid") long guestid, @RequestParam("arrival") String arrival, @RequestParam("departure") String departure, @RequestParam("roomtype") long roomid, Model model, RedirectAttributes redirectAttrs) {
+    if (hotels.exists(hotelid)) {
+      Hotel hotel = hotels.findOne(hotelid);
+
+      if (guests.exists(guestid)) {
+        Guest guest = guests.findOne(guestid);
+
+        if (rooms.exists(roomid)) {
+          Room room = rooms.findOne(roomid);
+
+          try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date dArrival = sdf.parse(arrival);
+            Date dDeparture = sdf.parse(departure);
+
+            Timestamp tArrival = new Timestamp(dArrival.getTime());
+            Timestamp tDeparture = new Timestamp(dDeparture.getTime());
+
+            if (tArrival.before(tDeparture)){
+              Booking booking = new Booking(tArrival, tDeparture, room.getType(), room, hotel, guest);
+              bookings.save(booking);
+
+              redirectAttrs.addFlashAttribute("message", "Booking created!");
+            } else {
+              redirectAttrs.addFlashAttribute("error", "Arrival date has to be before departure!");
+            }
+          } catch (Exception e) {
+            redirectAttrs.addFlashAttribute("error", "Dates are incorrect!");
+          }
+        } else {
+          redirectAttrs.addFlashAttribute("error", "Room doesn't exist!");
+        }
+
+      } else {
+        redirectAttrs.addFlashAttribute("error", "Guest doesn't exist!");
+      }
+
+    } else {
+      redirectAttrs.addFlashAttribute("error", "Hotel doesn't exist!");
+    }
+
     return "redirect:/dashboards/manager";
   }
 

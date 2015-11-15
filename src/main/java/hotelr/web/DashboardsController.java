5b0523243411b8b41d26817934e170506852304d
Gonçalo Cabrita <_@gmcabrita.com>
2015-11-15 16:39:49 +0000

    Add flash message to make redirection clear.

--- src/main/java/hotelr/web/DashboardsController.java
+++ src/main/java/hotelr/web/DashboardsController.java
@@ -13,8 +13,10 @@ import java.util.Map;
 public class DashboardsController {
   @RequestMapping(method=RequestMethod.GET)
   public String index(HttpServletRequest request, RedirectAttributes redirectAttrs) {
-    // TODO: pass received flash attributes to the next redirect
-    // System.out.println(((Map) RequestContextUtils.getInputFlashMap(request)));
+    Map<String, ?> flashMap = RequestContextUtils.getInputFlashMap(request);
+    if (flashMap != null && flashMap.containsKey("error")) {
+      redirectAttrs.addFlashAttribute("error", (String) flashMap.get("error"));
+    }
     if (request.isUserInRole("ROLE_GUEST")) return "redirect:/dashboards/guest";
     else if (request.isUserInRole("ROLE_MANAGER")) return "redirect:/dashboards/manager";
     else if (request.isUserInRole("ROLE_ADMIN")) return "redirect:/dashboards/admin";
